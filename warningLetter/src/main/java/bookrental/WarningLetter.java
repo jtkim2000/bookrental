35bb5a55284fa818b6a8193d6ffec3dced3b4486
jtkim2000 <82795757+jtkim2000@users.noreply.github.com>
2021-06-21 21:38:46 +0900

    Add files via upload

--- warningLetter/src/main/java/bookrental/WarningLetter.java
+++ warningLetter/src/main/java/bookrental/WarningLetter.java
@@ -13,10 +13,20 @@ public class WarningLetter {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long warningId;
     private Long memberId;
+
+    // 시스템의 환경변수인 configmap의 값("PleaseCareBook")을 가져와서 warningMsg 변수에 저장
+    private String warningMsg = System.getenv("configmap");
     private String status;
 
     @PostPersist
     public void onPostPersist(){
+        // 경고장 발송 위해 시스템 환경변수인 configmap의 값을 경고장 메시지에 담아 보낸다.
+        System.out.println("\n\n");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("##### get SYS ENV(configmap) -- : " + warningMsg);
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("\n\n");
+
         WarningSent warningSent = new WarningSent();
         BeanUtils.copyProperties(this, warningSent);
         warningSent.publishAfterCommit();
@@ -47,7 +57,13 @@ public class WarningLetter {
         this.status = status;
     }
 
+    public String getWarningMsg() {
+        return warningMsg;
+    }
 
+    public void setWarningMsg(String warningMsg) {
+        this.warningMsg = warningMsg;
+    }
 
 
 }
